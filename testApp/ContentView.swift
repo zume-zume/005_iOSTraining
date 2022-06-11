0b8637f7a22371278e75e54902cc6c02d6f3f769
“zumezume” <88725806+zume-zume@users.noreply.github.com>
2022-06-11 23:04:47 +0900

    Initial Commit

---
+++ testApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by zume on 2022/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by zume on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
