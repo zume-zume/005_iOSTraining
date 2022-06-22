85d8acb4afada08d9838f2266c1c19a1deda4f34
“zume-zume” <88725806+zume-zume@users.noreply.github.com>
2022-06-22 23:01:24 +0900

    画像の貼り方とか、解像度とかのお勉強

--- testApp/ContentView.swift
+++ testApp/ContentView.swift
@@ -10,11 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            Text("Turtle Rock")
-                .font(.title)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
         }
-            
-            
+        .padding()
     }
 }
 
