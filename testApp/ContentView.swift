8d9665cb943acfbf8120a544f45579ac41732d2e
“zume-zume” <88725806+zume-zume@users.noreply.github.com>
2022-06-25 21:37:55 +0900

    MapViewをカスタマイズ

--- testApp/ContentView.swift
+++ testApp/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+                
                 HStack {
                     Text("Joshua Tree National Park")
                         .font(.subheadline)
@@ -20,9 +29,20 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
