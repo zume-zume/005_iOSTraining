c712c4dc5b3e6c93f335b6cadbcadd3176a8419d
“zume-zume” <88725806+zume-zume@users.noreply.github.com>
2022-06-26 16:26:04 +0900

    エラー修正

---
+++ testApp/Views/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  testApp
+//
+//  Created by zume on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
